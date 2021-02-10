4c4cc947f755579b92590f83481a4c9ea7e21dbb
Bart Teeuwisse <bart@thecodemill.biz>
2021-02-10 14:47:42 -0800

    Create /webdav/audiobooks symlink

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,10 @@ fi
 
 echo "$ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY" > /etc/passwd-s3fs
 chmod 600 /etc/passwd-s3fs
+# Create a symlink so that path in the older entries.yaml files from when audiofiles were hosted
+# by transip.nl still work.
+mkdir -p /webdav
+ln -s /audiobooks /webdav/audiobooks
 
 s3fs icloud:/audiobooks /audiobooks -o passwd_file=/etc/passwd-s3fs -o url=$S3_ENDPOINT_URL -o nosuid,nonempty,nodev,allow_other,use_path_request_style,default_acl=private
 
