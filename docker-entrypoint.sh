b792695a064bcd8b1a77f2e0c02ac67c72ab2818
Bart Teeuwisse <bart@thecodemill.biz>
2021-04-21 11:52:31 -0700

    Don't list directories to avoid infinite recursion

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,7 +13,7 @@ chmod 600 /etc/passwd-s3fs
 # Create a symlink so that path in the older entries.yaml files from when audiofiles were hosted
 # by transip.nl still work.
 mkdir -p /webdav
-ln -s /audiobooks /webdav/audiobooks
+ln -sf /audiobooks /webdav/audiobooks
 
 s3fs icloud:/audiobooks /audiobooks -o passwd_file=/etc/passwd-s3fs -o url=$S3_ENDPOINT_URL -o nosuid,nonempty,nodev,allow_other,use_path_request_style,default_acl=private
 
