1684f1d0a1ff0f08e5dd55919f8a0435fe3f545e
Bart Teeuwisse <bart@thecodemill.biz>
2023-02-02 14:08:53 -0800

    Test DB model out in /ping

--- index.ts
+++ index.ts
@@ -3,9 +3,26 @@ import s3 from 's3';
 
 import { Author, Book, Category, CoverImage, knex, config, MediaFile } from './models/index.js';
 
-(async () => {
-  await knex.migrate.rollback({}, true)
-  await knex.migrate.latest(config.migrations)
+const server = fastify()
+
+const s3Client = s3.createClient({
+  s3Options: {
+    accessKeyId: "79DL49GLAYEZHOHO3NQ2",
+    secretAccessKey: "zrDcCBCqn3UasF1IXMKee6nXjd9fkz3MIGrOFYsP",
+    endpoint: "https://s3.wasabisys.com"
+  }
+})
+
+const lister = s3Client.listObjects({
+  s3Params: {
+      Bucket: "icloud",
+      Prefix: "audiobooks"
+  },
+  recursive: false
+})
+
+server.get('/ping', async (request, reply) => {
+  console.log(`Ping request received.`)
   const lee = Author.fromJson({
     name: "Lee Child",
   });
@@ -40,57 +57,19 @@ import { Author, Book, Category, CoverImage, knex, config, MediaFile } from './m
   console.log(JSON.stringify(book.categories, undefined, "  "))
   console.log(JSON.stringify(book.files, undefined, "  "))
   console.log(JSON.stringify(book.image, undefined, "  "))
-})()
 
-// const server = fastify()
+  return 'pong-a-long\n'
+})
 
-// const s3Client = s3.createClient({
-//   s3Options: {
-//     accessKeyId: "79DL49GLAYEZHOHO3NQ2",
-//     secretAccessKey: "zrDcCBCqn3UasF1IXMKee6nXjd9fkz3MIGrOFYsP",
-//     endpoint: "https://s3.wasabisys.com"
-//   }
-// })
+server.listen({ port: 8080 }, async (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
 
-// const lister = s3Client.listObjects({
-//   s3Params: {
-//       Bucket: "icloud",
-//       Prefix: "audiobooks"
-//   },
-//   recursive: false
-// })
+  // Setup the database
+  await knex.migrate.rollback({}, true)
+  await knex.migrate.latest(config.migrations)
 
-// interface IQuerystring {
-//   username: string;
-//   password: string;
-// }
-
-// interface IHeaders {
-//   'h-Custom': string;
-// }
-
-// server.get('/ping', async (request, reply) => {
-//   return 'pong-a-long\n'
-// })
-
-// server.get<{
-//   Querystring: IQuerystring,
-//   Headers: IHeaders
-// }>('/auth', {
-//   preValidation: (request, reply, done) => {
-//     const { username, password } = request.query
-//     done(username !== 'admin' ? new Error('Must be admin') : undefined) // only validate `admin` account
-//   }
-// }, async (request, reply) => {
-//   const customerHeader = request.headers['h-Custom']
-//   // do something with request data
-//   return `logged in!`
-// })
-
-// server.listen({ port: 8080 }, (err, address) => {
-//   if (err) {
-//     console.error(err)
-//     process.exit(1)
-//   }
-//   console.log(`Server listening at ${address}`)
-// })
+  console.log(`Server listening at ${address}`)
+})
