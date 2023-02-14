e267de22439f25958d585063aae5e70a8b4d421f
Bart Teeuwisse <bart@thecodemill.biz>
2023-02-13 22:34:52 -0800

    Created api.ts

---
+++ public/api.ts
@@ -0,0 +1,34 @@
+class Api {
+  static fetch(path: string): Promise<object> {
+    let result: string[] = [];
+
+    return fetch(`/api/v1/${path}`, {
+      headers: Api.headers
+    }).then((response) => response.body)
+      .then((body) => body?.getReader())
+      .then(async (reader) => {
+        await reader?.read().then(async function processResponse({ done, value }): Promise<Function|void> {
+          if (done) {
+            return
+          }
+    
+          // Convert the Uint8Array into text.
+          result.push(new TextDecoder().decode(value));
+    
+          // Read some more, and call this function again
+          return reader.read().then(processResponse);
+        })
+        return result
+      })
+      .then((result) => {
+        return JSON.parse(result.join(''))
+      })    
+  }
+  
+  private static headers = {
+    Authorization: 'Basic Ym9vazpyZWFkZXI='
+  }
+}
+
+export { Api }
+
