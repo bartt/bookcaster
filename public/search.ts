77c33af6af1b29118ebf123d7a11dc0e69f44b80
Bart Teeuwisse <bart@thecodemill.biz>
2023-02-14 19:34:48 -0800

    Fix history management

--- public/search.ts
+++ public/search.ts
@@ -12,7 +12,7 @@ declare module Search {
 
 class Search {
   corpus: Search.Book[];
-  input: Element|null;
+  input: HTMLInputElement | null;
   by: string;
 
   constructor(corpus: Search.Book[]) {
@@ -23,28 +23,47 @@ class Search {
 
   listen(): Search {
     this.input?.addEventListener('input', (e) => {
-      const phrase = e.target?.value
+      const url = new URL(window.location.href);
+      const phrase = (e.target as HTMLInputElement)?.value
+      // Show all books when there is no phrase. And remove the search query parameter.
+      if (phrase == '') {
+        for (let element of document.querySelectorAll('.book')) {
+          element.classList.remove('hidden');
+        }
+        if (url.searchParams.has('q')) {
+          url.searchParams.delete('q')
+          window.history.pushState({}, '', url)
+        }
+        return
+      }
+
+      // Set the search query parameter so that the search can be shared/bookmarked.
+      const q = url.searchParams.get('q')
+      if (q != phrase) {
+        url.searchParams.set('q', phrase)
+        window.history.pushState({}, '', url)
+      }
       const expr = new RegExp(`${phrase}`, 'i')
-      const hits = this.corpus.filter((book) => 
-        expr.test(book.title) || 
-        expr.test(book.description || '') || 
+      const hits = this.corpus.filter((book) =>
+        expr.test(book.title) ||
+        expr.test(book.description || '') ||
         book.authors.some((author) => expr.test(author.name)) ||
         book.categories.some((category) => expr.test(category.name)))
       const misses = this.corpus.filter((book) => !hits.includes(book))
       hits.forEach((book) => document.querySelectorAll(`#book-${book.id}`).forEach((element) => element.classList.remove('hidden')))
       misses.forEach((book) => document.querySelectorAll(`#book-${book.id}`).forEach((element) => element.classList.add('hidden')))
     })
+    window.onpopstate = this.loadQueryParam.bind(this)
     return this
   }
 
-  loadQueryParam(): Search {
+  loadQueryParam(e: Event | null) {
     const params = (new URL(document.location.href)).searchParams
     const query = params.get('q')
-    if (query && this.input) {
-      this.input.setAttribute('value', query)
+    if (this.input) {
+      this.input.value = query || ''
       this.input.dispatchEvent(new Event('input'))
     }
-    return this
   }
 }
 
