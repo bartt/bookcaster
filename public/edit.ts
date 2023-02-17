de3bcc436fac954ae6f6458d69b38bfbbacfc6da
Bart Teeuwisse <bart@thecodemill.biz>
2023-02-17 09:02:19 -0800

    Toggling editable content

--- public/edit.ts
+++ public/edit.ts
@@ -1,11 +1,29 @@
 class Edit {
   static handleClick() {
     document.addEventListener('click', (e) => {
-      // Clicking an editable element makes it editable and places the cursor in the editable content
-      if (e.target?.classList.contains('editable')) {
-        e.target.setAttribute('contenteditable', true);
-        e.target.focus();
+      // Clicking an editable element makes it editable and places the cursor
+      // in the editable content
+      const element = e.target as HTMLElement;
+      if (element.isContentEditable) {
+        return;
       }
+      // Make editable content editable
+      if (element?.getAttribute('contenteditable') == 'false') {
+        element.setAttribute('contenteditable', 'true');
+        element.focus();
+        return;
+      }
+      // New lines add new nodes inside the editable content. Clicks on children 
+      // make the editable content edtibable again.
+      const ancestor = element.closest('[contenteditable]');
+      if (ancestor && !ancestor.isContentEditable) {
+        ancestor.setAttribute('contenteditable', 'true');
+        return;
+      }
+      // Clicks outside editable content makes all editable content ineditable.
+      document
+        .querySelectorAll('[contenteditable]')
+        .forEach((item) => item.setAttribute('contenteditable', 'false'));
     });
   }
 }
